acd3ed031ef0152f362ba1188acf71688a42bc70
infosmitemain <infosmite.main@gmail.com>
2026-09-22 10:07:00 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,5 +2,5 @@ import streamlit as st
 s=st.selectbox("select your fav color",['green','red','blue'])
 st.write("my fav color is",s)
 st.write(f'my fav color is {s}')
-if st.button("click me");
+if st.button("click me"):
     st.balloons()
